8d8a616ceac7e392367619f361271b35bdeef4f4
Benjamin-Berard <benjaminb4@outlook.fr>
2024-11-13 12:23:21 +0100

    coverage des classes

--- src/test/java/fr/uniavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/uniavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -2,12 +2,16 @@ package fr.uniavignon.pokedex.api;
 import fr.univavignon.pokedex.api.IPokemonMetadataProvider;
 import fr.univavignon.pokedex.api.PokedexException;
 import fr.univavignon.pokedex.api.PokemonMetadata;
+import fr.univavignon.pokedex.api.PokemonMetadataProvider;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -19,11 +23,19 @@ public class IPokemonMetadataProviderTest {
     @Mock
     IPokemonMetadataProvider pokemonMetadataProvider;
 
+    PokemonMetadataProvider pokemonMetadataProvider1 = new PokemonMetadataProvider();
+
 
     PokemonMetadata pokemonMetadata;
+    PokemonMetadata pokemonMetadata1;
+    List<PokemonMetadata> pokemonMetadataList = new ArrayList<>();
     @BeforeEach
     void setUp() {
+        pokemonMetadata1 = new PokemonMetadata(1, "Aquali", 186, 168, 300);
+
         pokemonMetadata = new PokemonMetadata(0, "Bulbizarre", 126, 126, 90);
+        pokemonMetadataList.add(pokemonMetadata);
+        pokemonMetadataList.add(pokemonMetadata1);
     }
     @Test
     void shouldReturnPokemonMetadataWhenGetPokemonMetadata() throws PokedexException {
@@ -62,6 +74,17 @@ public class IPokemonMetadataProviderTest {
     @Test
     void shouldReturnCorrectStamina() {
         assertEquals(90, pokemonMetadata.getStamina());
+    }
+
+    @Test
+    void shouldReturnPokemonMetadataList(){
+        assertEquals(1,PokemonMetadataProvider.getPokemonMetadata().size());
+    }
+
+    @Test
+    void shouldReturn2(){
+       PokemonMetadataProvider.setPokemonMetadata(pokemonMetadataList);
+       assertEquals(2,PokemonMetadataProvider.getPokemonMetadata().size());
     }
 
 }
